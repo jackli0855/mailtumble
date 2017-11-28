88f649bd672d742e1f784fd2998689e0e4f87e40
libao <lb.funward@gmail.com>
2017-11-28 23:18:08 +0800

    fix issue that only one worker was lanched

--- send-out.js
+++ send-out.js
@@ -1,28 +1,37 @@
-'use strict';
-
 const AWS = require('aws-sdk')
 const SQS = new AWS.SQS({ apiVersion: '2012-11-05' })
 const SES = new AWS.SES()
 
-const QUEUE_URL = process.env.QUEUE_URL
+const QUEUE_URL = "https://sqs.us-west-2.amazonaws.com/103494865495/NeedSendEmailQuence"
 const MAXIMUM_SEND_RATE = 14
 
 function sendEmail(message) {
-    console.log(`Sending an email to: ${message.recipients.join(',')}`)
-
+    let data = JSON.parse(message.Body)
+    let subject = "A Message To You Rudy"
+    if (data.subject !== "") subject = data.subject
     var params = {
-        Destinations: message.recipients,
-        Source: message.originalRecipient,
-        RawMessage: {
-            Data: message.emailData
-        }
+        Destination: { ToAddresses: [data.to] },
+        Source: data.from,
+        Message: {
+           Subject: {
+              Data: subject
+           },
+           Body: {
+               Text: {
+                   Data: data.body,
+               }
+            }
+       }
     }
-
+    
     return new Promise(function(resolve, reject) {
-        SES.sendRawEmail(params, function(err) {
-            if (err) return reject(new Error("Error: Email sending failed."))
-
-            resolve(`Sent email successfully to ${message.recipients.join(',')}`)
+        SES.sendEmail(params, function(err, data) {
+            if (err) {
+               console.log(err)
+               reject(new Error("Error: Email sending failed."))
+             } else {
+               resolve("Sent email successfully to")
+             }
         })
     })
 }
@@ -60,11 +69,12 @@ function getQueueData() {
 }
 
 function pollIteration(results) {
+  // console.log('========== pollIteration ===========')
     const delay = 0
     const params = {
         QueueUrl: QUEUE_URL,
-        MaxNumberOfMessages: 5,
-        VisibilityTimeout: 10,
+        MaxNumberOfMessages: 10,
+        VisibilityTimeout: 5,
         WaitTimeSeconds: 5
     };
 
@@ -73,14 +83,14 @@ function pollIteration(results) {
             if (err) return reject(err);
 
             if (! data.Messages) {
-                setTimeout(() => resolve(results.concat('Queue seems to be empty')), delay)
+                setTimeout(() => resolve(results.concat('Queue seems to be empty!')), delay)
             } else {
                 const promises = data.Messages.map((message) => processMessage(message));
                 console.log(`${data.Messages.length} jobs received from the queue`)
 
                 // complete when all invocations have been made
                 Promise.series(promises).then(() => {
-                    const result = `Messages processed: ${data.Messages.length}`;
+                    const result = [`Messages processed: ${data.Messages.length}`, data.Messages.length];
                     console.log(result);
                     setTimeout(() => resolve(results.concat(result)), delay)
                 });
@@ -90,24 +100,39 @@ function pollIteration(results) {
 }
 
 function poll() {
+  // console.log('========== poll() ===========')
     const promises = Array(8).fill(pollIteration)
 
     return Promise.series(promises, [])
 }
 
 exports.handler = (event, context, callback) => {
+    // TODO implement
     try {
         // Run orchestration (invoked by schedule)
         getQueueData().then((numItems) => {
+            console.log(`========= all messages count: ${numItems}`)
             // Choose concurrency level
-            const concurrency = Math.min(MAXIMUM_SEND_RATE, Math.max(1, Math.round(parseInt(numItems) / 40)));
-            const promises = Array(concurrency).fill(poll())
-
+            const concurrency = Math.min(MAXIMUM_SEND_RATE, Math.max(1, Math.round(parseInt(numItems) / 15)));
+            var promises = Array(concurrency);
+            for(let i = 0; i < concurrency; i++){
+              promises[i] = poll();
+            }
+            // const promises = Array(concurrency).fill(poll())
+            console.log(promises)
             console.log(`Launching ${concurrency} workers`)
 
             Promise.all(promises).then((results) => {
-                console.log(results)
-                callback(null, results);
+                // console.log(results)
+                let totalMsg = results.reduce((accumulator, currentValue) => {
+                  if(Array.isArray(accumulator)){
+                    accumulator = accumulator[1] + currentValue[1]
+                  }else{
+                    accumulator += currentValue[1]  
+                  }
+                  return accumulator
+                });
+                callback(null, `end end end all messages count:${totalMsg}`);
             });
         })
     } catch (err) {
@@ -116,11 +141,13 @@ exports.handler = (event, context, callback) => {
 };
 
 Promise.series = function(promises, initValue) {
+  console.log('======== Promise.series')
     return promises.reduce(function(chain, promise) {
-        if (typeof promise !== 'function') {
-            return Promise.reject(new Error("Error: Invalid promise item: " +
-                promise));
-        }
+        console.log(promise)
+        // if (typeof promise !== 'function') {
+        //     return Promise.reject(new Error("Error: Invalid promise item: " +
+        //         promise));
+        // }
         return chain.then(promise);
     }, Promise.resolve(initValue));
 };
